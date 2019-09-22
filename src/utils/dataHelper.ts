bd4c663920643404c3ffd09f145e65eb693ab7d3
Ozan Manav <ozan.manavv@gmail.com>
2019-09-22 12:52:49 +0300

    promises

--- src/utils/dataHelper.ts
+++ src/utils/dataHelper.ts
@@ -3,88 +3,78 @@ import axios from 'axios';
 import { BASE_URL } from '../config';
 import { auth } from '../firebase';
 
-export const getRequest = async (url: any, params = {}) => {
-  await new Promise((resolve, reject) => {
-    auth
-      .onAuthStateChanged()
-      .then((user: any) =>
-        user
-          .getIdToken(false)
-          .then((token: string) => {
-            axios({
-              method: 'GET',
-              headers: { Authorization: `Bearer ${token}` },
-              baseURL: BASE_URL,
-              url,
-              params
-            })
-              .then(result => resolve(result))
-              .catch(error => reject(error.response.data));
+export function getRequest(url: any, params = {}) {
+  const baseURL = BASE_URL || '';
+  return new Promise((resolve, reject) => {
+    auth.onAuthStateChanged().then((user: any) => {
+      return user
+        .getIdToken(false)
+        .then((token: string) => {
+          axios({
+            method: 'GET',
+            headers: { Authorization: `Bearer ${token}` },
+            baseURL,
+            url,
+            params
           })
-          .catch((error: any) => {
-            reject(error);
-          })
-      )
-      .catch((error: any) => {
-        reject(error);
-      });
+            .then(result => resolve(result))
+            .catch(error => reject(error.response.data));
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
   });
-};
+}
 
-export const postRequest = async (url: any, params = {}, data: {}) => {
-  await new Promise((resolve, reject) => {
-    auth
-      .onAuthStateChanged()
-      .then((user: any) =>
-        user
-          .getIdToken(false)
-          .then((token: string) => {
-            axios({
-              method: 'POST',
-              headers: { Authorization: `Bearer ${token}` },
-              baseURL: BASE_URL,
-              url,
-              params,
-              data
-            })
-              .then(result => resolve(result))
-              .catch(error => reject(error.response.data));
+export function postRequest(url: any, params = {}, data: {}) {
+  const baseURL = BASE_URL || '';
+  return new Promise((resolve, reject) => {
+    auth.onAuthStateChanged().then((user: any) => {
+      return user
+        .getIdToken(false)
+        .then((token: string) => {
+          console.log('post token', token);
+          axios({
+            method: 'POST',
+            headers: { Authorization: `Bearer ${token}` },
+            baseURL,
+            url,
+            params,
+            data
           })
-          .catch((error: any) => {
-            reject(error);
-          })
-      )
-      .catch((error: any) => {
-        reject(error);
-      });
+            .then(result => resolve(result))
+            .catch(error => reject(error.response.data));
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
   });
-};
+}
 
-export const putRequest = async (url: any, params = {}, data: {}) => {
-  await new Promise((resolve, reject) => {
-    auth
-      .onAuthStateChanged()
-      .then((user: any) =>
-        user
-          .getIdToken(false)
-          .then((token: string) => {
-            axios({
-              method: 'PUT',
-              headers: { Authorization: `Bearer ${token}` },
-              baseURL: BASE_URL,
-              url,
-              params,
-              data
-            })
-              .then(result => resolve(result))
-              .catch(error => reject(error.response.data));
+export function putRequest(url: any, params = {}, data: {}) {
+  const baseURL = BASE_URL || '';
+  return new Promise((resolve, reject) => {
+    auth.onAuthStateChanged().then((user: any) => {
+      return user
+        .getIdToken(false)
+        .then((token: string) => {
+          console.log('post token', token);
+          axios({
+            method: 'PUT',
+            headers: { Authorization: `Bearer ${token}` },
+            baseURL,
+            url,
+            params,
+            data
           })
-          .catch((error: any) => {
-            reject(error);
-          })
-      )
-      .catch((error: any) => {
-        reject(error);
-      });
+            .then(result => resolve(result))
+            .catch(error => reject(error.response.data));
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
   });
-};
+}
