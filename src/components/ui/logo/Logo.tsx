48b6278ef601a3e567f77ee8c654ad6ab58d8f36
Ozan Manav <ozan.manavv@gmail.com>
2019-09-29 18:55:39 +0300

    logo cache cleaned

--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -1,6 +1,6 @@
 import React, { ImgHTMLAttributes, FunctionComponent } from 'react';
-import logo from './logo.svg';
+import LOGO from './logo_q.svg';
 
 export const Logo: FunctionComponent<ImgHTMLAttributes<HTMLImageElement>> = props => {
-  return <img src={logo} alt="feynlab logo" {...props} />;
+  return <img src={LOGO} alt="qubitro logo" {...props} />;
 };
