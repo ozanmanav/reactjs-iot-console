cf40b9846999ef633747672180df729fc25a12c8
Ozan Manav <ozan.manavv@gmail.com>
2019-10-13 22:17:18 +0300

    Dashboard charts SHOW

--- src/views/app/dashboard/Dashboard.tsx
+++ src/views/app/dashboard/Dashboard.tsx
@@ -13,12 +13,21 @@ import {
   Scatter,
   AreaChart,
   Area,
-  ResponsiveContainer
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar
 } from 'recharts';
 import rechartsDataLine from './rechartsData.json';
 import rechartsDataBar from './rechartsDataBar.json';
 import rechartsDataArea from './rechartsDataArea.json';
 import rechartsDataScatter from './rechartsDataScatter.json';
+import rechartsDataPie from './rechartsDataPie.json';
+import rechartsDataRadar from './rechartsDataRadar.json';
 import GridLayout from 'react-grid-layout';
 import './Dashboard.scss';
 import { useLocalStorage } from '../../../hooks';
@@ -27,8 +36,13 @@ const defaultLayout = [
   { i: 'a', x: 0, y: 0, w: 4, h: 6 },
   { i: 'b', x: 4, y: 0, w: 4, h: 6 },
   { i: 'c', x: 8, y: 0, w: 4, h: 6 },
-  { i: 'd', x: 0, y: 8, w: 4, h: 6 }
+  { i: 'd', x: 0, y: 8, w: 4, h: 6 },
+  { i: 'e', x: 4, y: 8, w: 4, h: 6 },
+  { i: 'f', x: 8, y: 8, w: 4, h: 6 }
 ];
+
+const primaryColor = '#F68A4D';
+const secondaryColor = '#4A4A4A';
 
 export const Dashboard: FunctionComponent = () => {
   const [savedLayout, saveLayout] = useLocalStorage('savedLayout', defaultLayout);
@@ -55,8 +69,8 @@ export const Dashboard: FunctionComponent = () => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              <Line type="monotone" dataKey="pv" stroke={primaryColor} activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="uv" stroke={secondaryColor} />
             </LineChart>
           </ResponsiveContainer>
         </div>
@@ -68,8 +82,8 @@ export const Dashboard: FunctionComponent = () => {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="pv" fill={primaryColor} />
+              <Bar dataKey="uv" fill={secondaryColor} />
             </BarChart>
           </ResponsiveContainer>
         </div>
@@ -80,7 +94,7 @@ export const Dashboard: FunctionComponent = () => {
               <XAxis dataKey="name" />
               <YAxis />
               <Tooltip />
-              <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+              <Area type="monotone" dataKey="Temp" fill={primaryColor} stroke={secondaryColor} />
             </AreaChart>
           </ResponsiveContainer>
         </div>
@@ -88,11 +102,37 @@ export const Dashboard: FunctionComponent = () => {
           <ResponsiveContainer width="99%">
             <ScatterChart>
               <CartesianGrid />
-              <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-              <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+              <XAxis type="number" dataKey="x" name="Amper" unit="Amp" />
+              <YAxis type="number" dataKey="y" name="Temprature" unit="Temp" />
               <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter name="A school" data={rechartsDataScatter} fill="#8884d8" />
+              <Scatter name="A school" data={rechartsDataScatter} fill={primaryColor} />
             </ScatterChart>
+          </ResponsiveContainer>
+        </div>
+        <div key="e">
+          <ResponsiveContainer width="99%">
+            <PieChart>
+              <Pie dataKey="value" isAnimationActive={false} data={rechartsDataPie} fill={primaryColor} label />
+
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div key="f">
+          <ResponsiveContainer width="99%">
+            <RadarChart data={rechartsDataRadar}>
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis />
+              <Radar
+                name="Watering Flowers"
+                dataKey="A"
+                stroke={secondaryColor}
+                fill={primaryColor}
+                fillOpacity={0.6}
+              />
+              <Legend />
+            </RadarChart>
           </ResponsiveContainer>
         </div>
       </GridLayout>
